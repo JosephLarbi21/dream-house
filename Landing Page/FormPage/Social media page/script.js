5b6318877256af1ba7fd663c95ff58f023773909
joey2022-eng <lawsonjoey546@gmail.com>
2023-05-07 20:59:51 +0000

    first push for review

---
+++ Landing Page/FormPage/Social media page/script.js
@@ -0,0 +1,6 @@
+var settingsmenu = document.querySelector(".seettings-menu")
+
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height")
+}
---
+++ Landing Page/FormPage/vendor/paasword-reset-code.php
@@ -0,0 +1,11 @@
+<?php
+include('dbcon.php'):
+
+if($_POST['password_reset_link']))
+{
+$email= mysqli_real_escape_string($con,$_POST['email']);
+
+
+
+$check_email= 
+}
